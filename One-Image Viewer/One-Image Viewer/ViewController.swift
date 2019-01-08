a50856179a19c14fef53d2e991bba90c51286dbd
Skuer <skuerth@gmail.com>
2019-01-08 10:31:42 +0800

    first commit

---
+++ One-Image Viewer/One-Image Viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  One-Image Viewer
+//
+//  Created by Skuerth on 2019/1/8.
+//  Copyright © 2019 Skuerth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
